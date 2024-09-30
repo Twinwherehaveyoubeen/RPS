f7314d31c9beffca275adc335ba607e4159caa57
Jose <jjmaximo13@gmail.com>
2024-09-30 10:05:06 -0400

    Just change the way i was going tto use Js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-const chooses = [ Rock, Paper, Scissors]
-const Playersresult = document. getElementById("PR")
-const Computersresult = document.getElementById("CPR")
+let playerscore = 0
+let computerscore = 0 
+let tie 
+
+function random(){
+    const chooses = [ "Rock" , "Paper", "Siccors"]
+    const random = Math.floor(Math.random() * 3)
+    return chooses[random]
+}
+
+
