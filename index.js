aacee2194e492b77d100823adbd3dc4376240a10
Jose <jjmaximo13@gmail.com>
2024-09-30 10:41:58 -0400

    function for the random selection and the function for the game

--- index.js
+++ index.js
@@ -3,9 +3,26 @@ let computerscore = 0
 let tie 
 
 function random(){
-    const chooses = [ "Rock" , "Paper", "Siccors"]
+    const chooses = [ "Rock" , "Paper", "Scissors"]
     const random = Math.floor(Math.random() * 3)
     return chooses[random]
 }
-
+function RPS(PC){
+    const computerchoice = random()
+    let result 
+     if ( PC === computerchoice ){
+            result = "Tie"
+            tie++
+     }else if (
+        (PC === 'Rock' && computerchoice ==='Scissors')||
+        (PC ==='Scissors' && computerchoice === 'Paper')||
+        (PC ==='Paper' && computerchoice === 'Scissors')
+     ){
+        result = "Players Point"
+        playerscore++
+     } else{
+            result = "Computers Point"
+            computerscore++
+     }
+}
 
